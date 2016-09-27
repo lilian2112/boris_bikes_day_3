e1dc4c8a56e158e6af12b03b168a3097d280d54f
Kyaw Tun Sein <kyawtunsein.gla@gmail.com>
2016-09-27 16:06:39 +0100

    Add lib

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,7 @@
-describe 'DockingStation' do
-  it 'releases bike when user asks' do
-    expect(release_bike(bike)).to eq 'Bike released'
-  end
+require 'docking_station'
+
+describe DockingStation do
+  #it 'releases bike when user asks' do
+    #expect(release_bike(bike)).to eq 'Bike released'
+  #end
 end
