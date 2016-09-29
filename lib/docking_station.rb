d2fa49eed012391230d5c4a2c4b148b3638409e9
Lilian Breidenbach <liliancbreidenbach@gmail.com>
2016-09-29 11:47:48 +0100

    storing up to 20 bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,18 +1,18 @@
 class DockingStation
-
-  attr_reader :bike
-
-  def release_bike
-    raise "sorry no bikes available" unless @bike
-    @bike
+  def initialize
+  @bikes = []
   end
-
+  attr_reader :bikes
+  def release_bike
+    raise "sorry no bikes available" unless @bikes.length >= 1
+    @bikes.pop
+  end
   def dock_bike(bike)
-    raise "sorry this station is full" if @bike
-    @bike = bike
+    raise "sorry this station is full" if @bikes.length >= 20
+    @bikes << bike
+    bike = @bikes.last
   end
 end
-
 class Bike
   def working?
     true
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,6 +2,8 @@ require 'docking_station'
 
 describe DockingStation do
     it {is_expected.to respond_to :release_bike}
+
+
     it 'releases previously docked bike' do
       bike = Bike.new
       subject.dock_bike(bike)
@@ -10,6 +12,9 @@ describe DockingStation do
     it "raise an error if there is no bikes" do
       expect { subject.release_bike }.to raise_error('sorry no bikes available')
     end
+
+
+
     it 'is working?' do
       expect(Bike.new.working?).to eq true
     end
@@ -18,16 +23,18 @@ describe DockingStation do
       bike = Bike.new
       expect(subject.dock_bike(bike)).to eq bike
     end
-    it 'raise an error if the station has one bike' do
-      bike = Bike.new
-      subject.dock_bike(bike)
-      expect { subject.dock_bike(bike)}.to raise_error('sorry this station is full')
+
+
+    it 'raise an error if the station has 20 bikes' do
+      20.times { subject.dock_bike(Bike.new) }
+      expect { subject.dock_bike(Bike.new)}.to raise_error('sorry this station is full')
     end
 
-    it {is_expected.to respond_to(:bike)}
-    it 'shows bike docked at station' do
-      bike = Bike.new
-      subject.dock_bike(bike)
-      expect(subject.bike).to eq bike
+
+
+    it {is_expected.to respond_to(:bikes)}
+    it 'shows number of bikes docked at station' do
+      2.times {subject.dock_bike(Bike.new)}
+      expect(subject.bikes.length).to eq 2
     end
 end
