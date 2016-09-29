23646f4451c5b51d4a77e24beed436c3f88f8eb8
Laura Weston <laura.weston@outlook.com>
2016-09-29 11:59:53 +0100

    Refactor release_bike and dock_bike methods by writing separate full? and empty? methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,15 +4,25 @@ class DockingStation
   end
   attr_reader :bikes
   def release_bike
-    raise "sorry no bikes available" unless @bikes.length >= 1
+    raise "sorry no bikes available" if empty?
     @bikes.pop
   end
   def dock_bike(bike)
-    raise "sorry this station is full" if @bikes.length >= 20
+    raise "sorry this station is full" if full?
     @bikes << bike
     bike = @bikes.last
   end
+
+  private
+  def full?
+    @bikes.length >= 20
+  end
+  def empty?
+    @bikes.empty?
+  end
+
 end
+
 class Bike
   def working?
     true
